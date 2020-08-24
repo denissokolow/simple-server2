5c15ae39704017f63e4c6333f227be0016b476dd
Денис <denis.sokolow@gmail.com>
2020-08-24 20:56:04 +0300

    master

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const moment = require('moment');
-
+const PORT = 4321;
 const server = http.createServer();
-server.listen(80);
+server.listen(process.env.PORT || PORT);
 server.on('request', (req, res) => {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.end(JSON.stringify({date: moment().format('DD.MM.YYYY HH.mm.ss')}));
